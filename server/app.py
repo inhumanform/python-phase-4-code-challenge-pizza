cd216d36140a31d23ff35d37bb586c10f9208c91
Drew <dch528@gmail.com>
2024-04-10 13:53:22 -0400

    Complete

--- server/app.py
+++ server/app.py
@@ -24,6 +24,79 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+class AllRestaurants(Resource):
 
-if __name__ == "__main__":
+    def get(self): 
+        restaurants = Restaurant.query.all()
+        response_body = [restaurant.to_dict(only=('id', 'name', 'address')) for restaurant in restaurants]
+        return make_response(response_body, 200)
+
+        # except:
+        #     response_body = {"errors": ['Restaurant Not Found']
+        #     }
+        #     return make_response(response_body, 400)
+        
+
+api.add_resource(AllRestaurants, '/restaurants')
+
+
+class RestaurantByID(Resource):
+
+    def get(self, id):
+        restaurants = Restaurant.query.filter(Restaurant.id == id).first()
+
+        if(restaurants):
+            response_body = restaurants.to_dict(rules=('-restaurant_pizzas.restaurants', 'restaurant_pizzas.pizzas.restaurant_pizzas'))
+            return make_response(response_body, 200)
+        else:
+            response_body = {
+                "error": "Restaurant not found"
+            }
+            return make_response(response_body, 404)
+        
+    def delete(self, id):
+        restaurants = db.session.get(Restaurant, id)
+
+        if(restaurants):
+            db.session.delete(restaurants)
+            db.session.commit()
+            response_body = {}
+            return make_response(response_body, 204)
+        else:
+            response_body = {
+                "error": "Restaurant not found"
+            }
+            return make_response(response_body, 404)
+
+api.add_resource(RestaurantByID, '/restaurants/<int:id>')    
+
+
+class AllPizzas(Resource):
+
+    def get(self):
+        pizzas = Pizza.query.all()
+        response_body = [pizza.to_dict(only=('id', 'name', 'ingredients')) for pizza in pizzas]
+        return make_response(response_body, 200)
+
+api.add_resource(AllPizzas, '/pizzas')
+
+
+class NewPizzas(Resource):
+
+    def post(self):
+        try:
+            new_pizza = RestaurantPizza(price=request.json.get('price'), pizza_id=request.json.get('pizza_id'), restaurant_id=request.json.get('restaurant_id'))
+            db.session.add(new_pizza)
+            db.session.commit()
+            response_body = new_pizza.to_dict(rules=('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas'))
+            return make_response(response_body, 201)
+        except:
+            response_body = {
+                "errors": ["validation errors"]
+            }
+            return make_response(response_body, 400)
+
+api.add_resource(NewPizzas, '/restaurant_pizzas')
+
+if __name__ == '__main__':
     app.run(port=5555, debug=True)
